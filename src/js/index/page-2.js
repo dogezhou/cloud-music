114ade26992af170e8ad99d874618d5746b416fe
Doge Zhou <doge.zhou@outlook.com>
2018-03-27 17:25:01 +0800

    热门页面

--- src/js/index/page-2.js
+++ src/js/index/page-2.js
@@ -1,9 +1,36 @@
 {
     let view = {
         el: '.page-2',
+        template: `
+          <li>
+            <h3>{{song.name}}</h3>
+            <p>
+              <svg class="icon icon-sq">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq"></use>
+              </svg>
+              {{song.singer}}
+            </p>
+            <a class="playButton" href="./song.html?id={{song.id}}">
+              <svg class="icon icon-play">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
+              </svg>
+            </a>
+          </li>        
+        `,
         init() {
             this.$el = $(this.el)
         },
+        render(data) {
+            let { songs } = data
+            songs.map((song) => {
+                let $li = $(this.template
+                    .replace('{{song.name}}', song.name)
+                    .replace('{{song.singer}}', song.singer)
+                    .replace('{{song.id}}', song.id)
+                )
+                this.$el.find('ol.hot-list').append($li)
+            })
+        },        
         show() {
             this.$el.addClass('active')
         },
@@ -11,13 +38,29 @@
             this.$el.removeClass('active')
         }
     }
-    let model = {}
+    let model = {
+        data: {
+            songs: []
+        },
+        find() {
+            var query = new AV.Query('Song');
+            return query.find().then((songs) => {
+                this.data.songs = songs.map((song) => {
+                    return { id: song.id, ...song.attributes }
+                })
+                return songs
+            })
+        }
+    }
     let controller = {
         init(view, model) {
             this.view = view
             this.view.init()
             this.model = model
             this.bindEventHub()
+            this.model.find().then(() => {
+                this.view.render(this.model.data)
+            })            
         },
         bindEventHub() {
             window.eventHub.on('selectTab', (tabName) => {
